ab653fa5b6fad991196b99fb441cf9c7106342a7
Yevhen Osyshniuk <jekarabota1@gmail.com>
2022-09-08 20:43:38 +0300

    Lesson-10, work with Maps version #2

--- src/ua/ithillel/homework10/FileNavigator.java
+++ src/ua/ithillel/homework10/FileNavigator.java
@@ -2,21 +2,23 @@ package ua.ithillel.homework10;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class FileNavigator {
     private final Map<String, List<FileData>> fileDataMap = new HashMap<>();
 
     public void add(String path, FileData file) throws IllegalAccessException {
         if (!file.path().equals(path)) {
-            throw new IllegalAccessException("Wrong path");
-        } else {
-            if (fileDataMap.containsKey(path)) {
-                fileDataMap.get(path).add(file);
-            } else {
-                this.fileDataMap.put(path, Stream.of(file).collect(Collectors.toList()));
-            }
+            throw new IllegalAccessException("Wrong path to adding new file, paths are not equal");
         }
+        if (fileDataMap.containsKey(path)) {
+            fileDataMap.get(path).add(file);
+        } else {
+            List<FileData> newList = new ArrayList<>();
+            newList.add(file);
+            fileDataMap.put(path, newList);
+        }
+
     }
 
     public List<FileData> find(String path) {
