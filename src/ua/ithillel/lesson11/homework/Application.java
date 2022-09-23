0d5c87e82794ac450465babf1baa3bdf220d7089
Yevhen Osyshniuk <jekarabota1@gmail.com>
2022-09-23 22:41:27 +0300

    Lesson-11, work with Exceptions ver 2

--- src/ua/ithillel/lesson11/homework/Application.java
+++ src/ua/ithillel/lesson11/homework/Application.java
@@ -5,11 +5,11 @@ import ua.ithillel.lesson11.homework.exeptions.ArraySizeException;
 
 public class Application {
     public static void main(String[] args) {
-        String [][] data1 = {{"1","2","3","4"},{"5","6","7","8"},{"9","10","11","12"},{"13","14","15","16"}};
+        String[][] data1 = {{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}};
         ArrayValueCalculator arrayValueCalculator = new ArrayValueCalculator();
         try {
             System.out.println(arrayValueCalculator.doCalc(data1));
-        } catch (ArraySizeException | ArrayDataException exception){
+        } catch (ArraySizeException | ArrayDataException exception) {
             System.out.println(exception.getMessage());
         }
 
--- src/ua/ithillel/lesson11/homework/ArrayValueCalculator.java
+++ src/ua/ithillel/lesson11/homework/ArrayValueCalculator.java
@@ -3,13 +3,17 @@ package ua.ithillel.lesson11.homework;
 import ua.ithillel.lesson11.homework.exeptions.ArrayDataException;
 import ua.ithillel.lesson11.homework.exeptions.ArraySizeException;
 
+import java.util.Arrays;
+
 public class ArrayValueCalculator {
 
     private int numbersSum = 0;
 
     public int doCalc(String[][] data) {
-        if (!(data.length == 4) || !(data[0].length == 4)) {
-            throw new ArraySizeException("Array size is not 4*4, current size is: " + data.length + "x" + data[0].length);
+        if (Arrays.stream(data)
+                .filter(item -> item.length == 4)
+                .count() != 4) {
+            throw new ArraySizeException("Array size is not 4*4");
         } else {
             for (int i = 0; i < data.length; i++) {
                 for (int j = 0; j < data.length; j++) {
