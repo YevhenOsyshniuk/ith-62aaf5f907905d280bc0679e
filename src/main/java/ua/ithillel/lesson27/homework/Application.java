f91def877b756e878e87d2001048387d62128eee
Yevhen Osyshniuk <jekarabota1@gmail.com>
2023-01-06 21:12:54 +0200

    Lesson-27, work with Spring HW1 version 2

--- src/main/java/ua/ithillel/lesson27/homework/Application.java
+++ src/main/java/ua/ithillel/lesson27/homework/Application.java
@@ -2,29 +2,13 @@ package ua.ithillel.lesson27.homework;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ua.ithillel.lesson27.homework.entity.Product;
 import ua.ithillel.lesson27.homework.service.CartService;
 import ua.ithillel.lesson27.homework.service.ProductRepository;
 
 public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext("ua.ithillel.lesson27.homework.service");
-        Product product1 = new Product(1, "name 1", 10);
-        Product product2 = new Product(2, "name 2", 20);
-        Product product3 = new Product(3, "name 3", 30);
-        ProductRepository productRepository = new ProductRepository();
-        productRepository.add(product1);
-        productRepository.add(product2);
-        productRepository.add(product3);
-
-        CartService cart = new CartService(productRepository);
-        cart.addProduct(product1.getId());
-        cart.addProduct(product2.getId());
-        cart.addProduct(product3.getId());
-        System.out.println(cart.addProduct(1));
-        System.out.println(cart.removeProduct(1));
-
-        System.out.println(applicationContext.getBean(CartService.class));
-        System.out.println(applicationContext.getBean(CartService.class));
+        ProductRepository productRepository = applicationContext.getBean(ProductRepository.class);
+        CartService cart = applicationContext.getBean(CartService.class);
     }
 }
--- src/main/java/ua/ithillel/lesson27/homework/service/ProductRepository.java
+++ src/main/java/ua/ithillel/lesson27/homework/service/ProductRepository.java
@@ -2,15 +2,25 @@ package ua.ithillel.lesson27.homework.service;
 
 import org.springframework.stereotype.Component;
 import ua.ithillel.lesson27.homework.entity.Product;
-
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Component
 public class ProductRepository {
 
     private final List<Product> productsInRepos = new ArrayList<>();
+
+    @PostConstruct
+    private void postConstruct() {
+        Product product1 = new Product(1, "name 1", 10);
+        Product product2 = new Product(2, "name 2", 20);
+        Product product3 = new Product(3, "name 3", 30);
+        productsInRepos.add(product2);
+        productsInRepos.add(product1);
+        productsInRepos.add(product3);
+        System.out.println("Products : " + product1 + " " + product2 + " " + product3 + " added");
+    }
 
     public void add(Product product) {
         productsInRepos.add(product);
@@ -20,16 +30,16 @@ public class ProductRepository {
         productsInRepos.remove(product);
     }
 
-    public Optional<Product> getProduct(int id) {
+    public Product getProductById(long id) {
         for (Product product : productsInRepos) {
             if (product.getId() == id) {
-                return Optional.of(product);
+                return product;
             }
         }
-        return Optional.empty();
+        return null;
     }
 
     public List<Product> getAllProducts() {
-        return productsInRepos;
+        return productsInRepos.stream().toList();
     }
 }
